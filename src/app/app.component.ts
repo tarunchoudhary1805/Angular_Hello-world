6227b14c85aec7b61a06973185eb2055136a6d32
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2023-08-22 17:36:17 +0530

    Initial State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './app.service';
+import { AuthorsService } from './authors.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl:'./app.component.html' ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  email = 'a@gmail.com';
+  onKeyUp(){
+    console.log(`Enter Pressed  + ${this.email}`);
+    
+  }
 }
